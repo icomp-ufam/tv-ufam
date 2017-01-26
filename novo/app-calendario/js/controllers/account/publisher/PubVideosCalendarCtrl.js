d1af39caf55584531a1b4398bc636584408e02ad
Pedro Frota <pvmf@icomp.ufam.edu.br>
2017-01-26 03:52:24 -0400

    Modificações na view de Visualizar Programa

--- novo/app-calendario/js/controllers/account/publisher/PubVideosCalendarCtrl.js
+++ novo/app-calendario/js/controllers/account/publisher/PubVideosCalendarCtrl.js
@@ -21,12 +21,17 @@ angular.module("icomptvApp")
 
     $scope.horas = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
 
+    
+    //Representa a programação de uma semana inteira
     $scope.horarios = [];
 
     for (i=0, len = $scope.horas.length; i<len; ++i) {
       $scope.horarios.push(
                                 {
+                                  //Representa o horário que o programa passa
                                   horario: $scope.horas[i],
+
+                                  //Representa todos os dias da semana, começando por Domingo e terminando no Sábado.
                                   diasSemana: [[], [], [], [], [], [], []]
                                 }
                               );
--- novo/app-calendario/js/controllers/account/publisher/PubVideosViewCtrl.js
+++ novo/app-calendario/js/controllers/account/publisher/PubVideosViewCtrl.js
@@ -11,7 +11,9 @@ angular.module("icomptvApp")
   ) {
     AppLogSvc.log("PubVideosViewCtrl iniciado.");
 
-    $scope.program = {};
+    $desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum sapien tortor, vel feugiat eros dapibus eget. Vestibulum ligula quam, commodo eget commodo ut, auctor in elit. Aenean mattis tincidunt lectus, vel dictum neque convallis in. Aliquam posuere tortor vel augue dictum cursus. Proin commodo neque quis sapien sodales, sed mollis magna congue. Etiam in turpis eu ante condimentum scelerisque vel nec tortor. Sed sed odio non justo dictum iaculis vitae a magna. Proin cursus sed sapien vehicula molestie. Donec nulla ex, ornare in fermentum luctus, feugiat in magna. Sed at luctus leo. Nullam tempor pellentesque metus, sed gravida diam sodales vitae. Vivamus posuere eu ligula non dignissim. Vivamus eu quam ante. Quisque finibus sollicitudin justo a congue. Integer ut commodo lacus.";
+
+    $scope.programa = {inic: "13:00", term: "13:45", tit: "Hello World!", desc: $desc};
 
     $scope.app = "Visualizar programa";
   });
